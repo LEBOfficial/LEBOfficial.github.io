81e6b45d328f03ece7b428870bba5a3da8f65a22
LEB <88503645+LEBOfficial@users.noreply.github.com>
2023-05-06 22:46:05 +0200

    UPDATED WOOO

--- index.js
+++ index.js
@@ -3,42 +3,126 @@ function switch_appearance()
     document.body.classList.toggle("light-theme");
 }
 
-//#region TYPEWRITER I'M _______
-var texts = ["Game Developer", "Artist", "Musician", "I'm LEB"];
-var currentText = 0;
-var textPosition = 0;
+//#region NAV BUTTONS / SCROLL
+const nav_buttons = document.getElementsByClassName("nav-btn");
 
-var typing_speed = 100;
-var deleting_speed = 50;
-var waiting_speed = 800;
+let currentSection = 0;
 
-var deleting = false;
+const sections = 
+["home-section", 
+"game-section",
+"contact-section"];
 
-typer = () => {
-    document.getElementById("msg").innerHTML = texts[currentText].substring(0, textPosition) + '<span id="im-cursor">\u25ae</span>';
 
-    if (textPosition++ != texts[currentText].length) {
-        setTimeout(typer, typing_speed);
-    } 
-    else 
-    {
-        if (currentText == 3) return;
-        deleting = true;
-        setTimeout(deleter, waiting_speed);
-    }
+
+function nav_btn_click(btn) {
+
+    update_nav_btn(btn);
+
+    //GETTING THE BUTTONS SECTIONS
+    scrollElement(nav_buttons[btn].dataset.section);
+
+    currentSection = btn;
 }
-deleter = () => {
-    document.getElementById("msg").innerHTML = texts[currentText].substring(0, textPosition) + '<span id="im-cursor">\u21e6</span>';
-    if (textPosition-- != 0) {
-        setTimeout(deleter, deleting_speed);
+
+function update_nav_btn(btn) {
+    //SELECTING
+    for (let i = 0; i < nav_buttons.length; i++)
+    {
+        nav_buttons[i].classList.remove("selected");
+    }
+    nav_buttons[btn].classList.add("selected");
+}
+
+var scrollPercent;
+
+function scrollElement(sect) {
+    document.getElementById(sect).scrollIntoView({block: "start", behavior: "smooth"})
+    
+}
+
+window.addEventListener("scroll", function (event) {
+
+    /*
+    const homeRect = this.document.getElementById("home-section").getBoundingClientRect()
+    const gameRect = this.document.getElementById("game-section").getBoundingClientRect()
+    const contactRect = this.document.getElementById("contact-section").getBoundingClientRect()
+
+    
+    if (this.window.scrollY < gameRect.top)
+    {
+        update_nav_btn(0);
+    }
+    else if (this.window.scrollY > gameRect.top && this.window.scrollY < contactRect.bottom)
+    {
+        update_nav_btn(1);
     }
     else {
-        deleting = false;
-        setTimeout(typer, waiting_speed);
+        update_nav_btn(2);
+    }*/
 
-        currentText++;
+    scrollPercent = (document.documentElement.scrollTop||document.body.scrollTop) / ((document.documentElement.scrollHeight||document.body.scrollHeight) - document.documentElement.clientHeight) * 100;
+
+    if (scrollPercent < 30)
+    {
+        update_nav_btn(0);
     }
+    else if (scrollPercent > 30 && scrollPercent < 70)
+    {
+        update_nav_btn(1);
+    }
+    else
+    {
+        update_nav_btn(2);
+    }
+
+})
+
+//#endregion
+
+const clipboard_mouse = document.getElementById("clipboard-mouse")
+
+
+function discord_profile_click()
+{
+    console.log("Copied")
+    navigator.clipboard.writeText("LEB#3729");
+
+    clipboard_mouse.classList.remove("clipboard-active");
+    void clipboard_mouse.offsetWidth;
+    clipboard_mouse.classList.add("clipboard-active");
 }
 
-window.addEventListener("load", typer());
-//#endregion
+const nav_menu = document.getElementById("nav-menu")
+const nav_menu_opener = document.getElementById("nav-menu-opener")
+const nav_btns = document.getElementsByClassName("nav-btn")
+window.addEventListener("DOMContentLoaded", function (event) {
+    if (this.window.innerWidth <= 600)
+    {
+        for (let i = 0; i < nav_btns.length; i++)
+        {
+            nav_btns[i].style.display = "none";
+        }
+
+    }
+    else {
+        nav_menu_opener.style.display = "none";
+    }
+    nav_menu.style.display = "none";
+});
+
+let menu_open = false;
+
+nav_menu_opener.addEventListener("click", function (event) {
+    if (!menu_open)
+    {
+        nav_menu.style.display = "flex";
+        nav_menu.classList.remove("nav-menu-active")
+        void nav_menu.offsetWidth;
+        nav_menu.classList.add("nav-menu-active")
+    }
+    else {
+        nav_menu.style.display = "none";
+    }
+    menu_open = !menu_open
+})
